c8e7aab0152980853999be156c1f1cda6e9c8ede
renandfe22 <lealrenan@hotmail.com>
2024-09-10 22:53:20 -0300

    formulario modificado

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -36,37 +36,3 @@ document.addEventListener('DOMContentLoaded', () => {
         mainContent.classList.add('visible');
     });
 });
-
-//validação do formulario!
-document.getElementById('contact-form').addEventListener('submit', function(event) {
-    const name = document.getElementById('name').value.trim();
-    const email = document.getElementById('e-mail').value.trim();
-    const message = document.getElementById('message').value.trim();
-
-    // Validação do nome (se preenchido)
-    if (name !== '' && name.length === 0) {
-        alert('O campo nome não pode estar vazio.');
-        event.preventDefault(); // Impede o envio do formulário
-        return;
-    }
-
-    // Validação do e-mail (se preenchido)
-    if (email !== '' && !validateEmail(email)) {
-        alert('Por favor, insira um e-mail válido.');
-        event.preventDefault(); // Impede o envio do formulário
-        return;
-    }
-
-    // Validação da mensagem
-    if (message.length > 300) {
-        alert('A mensagem não pode ter mais de 300 caracteres.');
-        event.preventDefault(); // Impede o envio do formulário
-        return;
-    }
-});
-
-// Função para validar o formato do e-mail
-function validateEmail(email) {
-    const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return re.test(email);
-}
